1cbc965cec627d9449d15b18b107a52621286d95
mad-hat-ter <miss.sadov@mail.ru>
2026-03-17 15:11:00 +0300

    Итоговый проект

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,15 +1,25 @@
 
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button, Box, Badge, Container } from '@mui/material';
-import { NavLink, useLocation } from 'react-router-dom';
+import React, { useState } from 'react'; 
+import {
+  AppBar, Toolbar, Typography, Button, Box, Badge, Container,
+  IconButton, Drawer, List, ListItemButton, ListItemText
+} from '@mui/material';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom'; 
 import { useSelector } from 'react-redux';
 import { type RootState } from '../store';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import MovieIcon from '@mui/icons-material/MovieFilter';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Header: React.FC = () => {
   const location = useLocation();
+  const navigate = useNavigate(); 
   const favoriteCount = useSelector((state: RootState) => state.favorites.length);
+  const [mobileOpen, setMobileOpen] = useState(false); 
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
 
   const getNavLinkStyle = (path: string) => ({
     textDecoration: 'none',
@@ -21,11 +31,45 @@ const Header: React.FC = () => {
     },
   });
 
+
+  const getDrawerItemStyle = (path: string) => ({
+    color: location.pathname === path ? '#ffab00' : '#333', 
+    fontWeight: location.pathname === path ? 'bold' : 'normal',
+  });
+
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2, color: '#333' }}>
+        KINO-POISK
+      </Typography>
+      <List>
+        <ListItemButton onClick={() => navigate('/movies')}>
+          <ListItemText primary="Список фильмов" sx={getDrawerItemStyle('/movies')} />
+        </ListItemButton>
+        <ListItemButton onClick={() => navigate('/search')}>
+          <ListItemText primary="Поиск" sx={getDrawerItemStyle('/search')} />
+        </ListItemButton>
+        <ListItemButton onClick={() => navigate('/favorites')}>
+          <ListItemText primary="Избранное" sx={getDrawerItemStyle('/favorites')} />
+        </ListItemButton>
+      </List>
+    </Box>
+  );
+
   return (
     <AppBar position="sticky" sx={{ backgroundColor: '#1a1a1a' }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { md: 'none' }, color: '#ffab00' }} // Скрыть на md+
+          >
+            <MenuIcon />
+          </IconButton>
           <MovieIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1, color: '#ffab00' }} />
           <Typography
             variant="h6"
@@ -34,31 +78,23 @@ const Header: React.FC = () => {
             to="/"
             sx={{
               mr: 4,
-              display: { xs: 'none', md: 'flex' },
+              display: { xs: 'none', md: 'flex' }, 
               fontFamily: 'monospace',
               fontWeight: 700,
               letterSpacing: '.2rem',
               color: 'inherit',
               textDecoration: 'none',
+              flexGrow: 1, 
             }}
           >
           </Typography>
-
-
-          <Box sx={{ flexGrow: 1, display: 'flex' }}>
-            <Button component={NavLink} to="/" sx={getNavLinkStyle('/')}>
-                Главная
-            </Button>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}> 
             <Button component={NavLink} to="/movies" sx={getNavLinkStyle('/movies')}>
               Список фильмов
             </Button>
             <Button component={NavLink} to="/search" sx={getNavLinkStyle('/search')}>
               Поиск
             </Button>
-          </Box>
-
-
-          <Box sx={{ flexGrow: 0 }}>
             <Button
               component={NavLink}
               to="/favorites"
@@ -74,6 +110,20 @@ const Header: React.FC = () => {
           </Box>
         </Toolbar>
       </Container>
+      <Drawer
+        variant="temporary"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true, 
+        }}
+        sx={{
+          display: { xs: 'block', md: 'none' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: 240 }, 
+        }}
+      >
+        {drawer}
+      </Drawer>
     </AppBar>
   );
 };
